da86c8e76c7ca9e02ec2543edab252415623ea6a
Daniel Bezerra <danielxt499@gmail.com>
2025-12-14 13:14:21 -0300

    Slavar

--- frontend/nova-vite-project/vite.config.js
+++ frontend/nova-vite-project/vite.config.js
@@ -3,9 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/Nova-Coworking/",
+  plugins: [react()],
   build: {
     outDir: "../../docs",
     emptyOutDir: true,
   },
-  plugins: [react()],
 });
