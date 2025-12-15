3769591ee355f0cd979de8eaf1b7531f2efbb811
Daniel Bezerra <danielxt499@gmail.com>
2025-12-15 15:16:22 -0300

    S

--- backend/main.py
+++ backend/main.py
@@ -5,16 +5,11 @@ from router.usuarios import router as usuarios_router
 from router.salas import router as salas_router
 from router.reservas import router as reservas_router
 
-app = FastAPI(title="Nova Coworking API")
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://nova-coworking.vercel.app",
-        "http://localhost:5173",
-        "httop://nova-coworking-mvki6k79c-daniel-henriques-projects-2d0b8166.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],  # depois você restringe
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -25,4 +20,4 @@ app.include_router(reservas_router)
 
 @app.get("/")
 def health():
-    return {"status": "API Nova Coworking rodando"}
+    return {"status": "ok"}
