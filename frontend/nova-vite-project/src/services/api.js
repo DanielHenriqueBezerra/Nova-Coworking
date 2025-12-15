421c86f258ffdbb3e5d93efbe61253e54f687baa
DanielHenriqueBezerra <danielxt499@gmail.com>
2025-12-15 08:30:47 -0300

    Update api.js

--- frontend/nova-vite-project/src/services/api.js
+++ frontend/nova-vite-project/src/services/api.js
@@ -1,3 +1,7 @@
+import axios from "axios";
+
 const api = axios.create({
-  baseURL: "https://nova-coworking.onrender.com"
+  baseURL: "https://nova-coworking.onrender.com",
 });
+
+export default api;
